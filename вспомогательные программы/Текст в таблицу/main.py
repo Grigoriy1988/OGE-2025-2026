6bc64728b21dec8f6177b780b7575b8533504051
Prepod <bg_1988@mail.ru>
2025-10-08 09:54:06 +1200

    25
    Signed-off-by: Prepod <bg_1988@mail.ru>

---
+++ вспомогательные программы/Текст в таблицу/main.py
@@ -0,0 +1,11 @@
+with open("a.txt", "r", encoding='utf-8') as file:
+    s = file.read()
+print(s)
+answer = open('output.txt', mode='w')
+s2 = ''
+for i in s[0:-1]:
+    s2 += i + '\t'
+s2 += s[-1]
+answer.write(s2)
+answer.close()
+print(s2)
