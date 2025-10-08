25f84781ce5738cd01f00fb8c4d40b7c4db143fa
Prepod <bg_1988@mail.ru>
2025-10-08 13:35:52 +1200

    25
    Signed-off-by: Prepod <bg_1988@mail.ru>

--- вспомогательные программы/Текст в таблицу/main.py
+++ вспомогательные программы/Текст в таблицу/main.py
@@ -1,7 +1,7 @@
 with open("a.txt", "r", encoding='utf-8') as file:
     s = file.read()
 print(s)
-answer = open('output.txt', mode='w')
+answer = open('output.txt', mode='w', encoding="utf-8")
 s2 = ''
 for i in s[0:-1]:
     s2 += i + '\t'
