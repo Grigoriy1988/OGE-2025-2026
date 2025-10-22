c0fa28be5333588ccdefa21733c3dbcd9e18b2e6
Prepod <bg_1988@mail.ru>
2025-10-23 10:30:07 +1200

    15

---
+++ 03/001 0A6843.py
@@ -0,0 +1,5 @@
+for x in range(100,1000):
+    if (x % 10 == 3) and not(x < 230):
+        print(x)
+
+
---
+++ 03/002 09B748.py
@@ -0,0 +1,5 @@
+for x in range(1,1000):
+    if ((x > 3) and not(x < 4)) or (x < 1):
+        print(x)
+
+
