edf3a3ca76e6ac1d7d5acafb31b4fa867c0648d8
Prepod <bg_1988@mail.ru>
2025-11-10 15:48:21 +1200

    15

---
+++ 03/Все задачи/12 Номер 1A3B0C.py
@@ -0,0 +1,3 @@
+for x in range(1, 100):
+    if not(not (x < 8) or (x < 7)):
+        print(x)
---
+++ 03/Все задачи/12 Номер 2AD501.py
@@ -0,0 +1,4 @@
+for x in range(1, 100):
+    if not((x >= 3) or not (x ≥ 2)):
+        print(x)
+        break
