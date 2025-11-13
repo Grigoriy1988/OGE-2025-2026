b2608bcd8571218bd1331e8c7dae726f1630a488
bg_19 <bg_1988@mail.ru>
2025-11-14 00:45:33 +1200

    2025

--- 05/000 C80CF5.py
+++ 05/000 C80CF5.py
@@ -1,13 +1,16 @@
 def Вычислитель(a, b, step, k=''):
-    k = k + f'{a}'
-    if step == 0:
-        print(k + '  Ошибка' if a != b else k + "   ОТВЕТ")
+    # k = k + f'{a}'
+    if step == 0 and a != b:
+        #print(k + '  Ошибка' if a != b else k + "   ОТВЕТ")
+        return None
+    if step == 0 and a == b:
+        print(k)
         return None
     if a % 3 == 0:
-        Вычислитель(a * 10 + 1, b, step - 1, k + f'[1]->')
-        Вычислитель(a // 3, b, step - 1, k + f'[2]->')
+        Вычислитель(a * 10 + 1, b, step - 1, k + '1')  # + f'[1]->
+        Вычислитель(a // 3, b, step - 1, k + '2')
     else:
-        Вычислитель(a * 10 + 1, b, step - 1, k + f'[1]->')
+        Вычислитель(a * 10 + 1, b, step - 1, k + '1')
 
 
 Вычислитель(5, 19, 5)
