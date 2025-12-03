70eb70d271a916a353423702074783f88f851527
Prepod <bg_1988@mail.ru>
2025-12-03 16:05:41 +1200

    15

---
+++ 05/Все задачи/077 Номер 965F06.py
@@ -0,0 +1,11 @@
+def Квадратор(a, b, step, k=''):
+    if step == 0:
+        print(k + f" => {a}" + '  Ошибка' if a != b else k + f" => {a}" + "   ОТВЕТ")
+        return
+    if a > 27:
+        return
+    Квадратор(a ** 2, b, step - 1, k + '1')
+    Квадратор(a + 1, b, step - 1, k + '2')
+
+
+Квадратор(2, 27, 5)
