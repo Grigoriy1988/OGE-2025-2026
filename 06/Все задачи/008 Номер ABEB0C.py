0a8b4da1258ac5d7c68dc22120e11d355bf1eaf0
bg_19 <bg_1988@mail.ru>
2025-12-07 15:58:30 +1200

    2025

---
+++ 06/Все задачи/008 Номер ABEB0C.py
@@ -0,0 +1,16 @@
+list_s_t = [(1, 2), (11, 2), (1, 12), (11, 12), (-11, -12), (-11, 12), (-12, 11), (10, 10), (10, 5)]
+count = 0
+for A in range(-100, 100):
+    count = 0
+    for s, t in list_s_t:
+        if (s > 10) or (t > A):
+            count += 1
+            #print("YES")
+        else:
+            pass
+            #print("NO")
+
+    if count == 7:
+        print(f'ответ: {A}')
+        break
+
