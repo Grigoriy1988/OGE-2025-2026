14d8e632ed3894bc1b521b1c145fd8fc3e6a08c9
bg_19 <bg_1988@mail.ru>
2025-12-07 21:55:37 +1200

    2025

---
+++ 06/Все задачи/014 Номер 3C5AB1.py
@@ -0,0 +1,16 @@
+list_s_t = [(13, 2), (11, 12), (-12, 12), (2, -2), (-10, -10), (6, -5), (2, 8), (9, 10), (1, 13)]
+count = 0
+list_A = []
+for A in range(-100, 100):
+    count = 0
+    for s, t in list_s_t:
+        if(s > A) or (t > 12):
+            count += 1
+            # print("YES")
+        else:
+            pass
+            #print("NO")
+
+    if count == 4:
+        list_A.append(A)
+print(f'Ответ: {min(list_A)}')
