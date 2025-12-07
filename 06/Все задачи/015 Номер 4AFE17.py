78c53c3b125aeb562e0ad459b0a3ff690fb25724
bg_19 <bg_1988@mail.ru>
2025-12-07 21:59:10 +1200

    2025

---
+++ 06/Все задачи/015 Номер 4AFE17.py
@@ -0,0 +1,16 @@
+list_s_t = [(-9, 11), (2, 7), (5, 12), (2, -2), (7, -9), (12, 6), (9, -1), (7, 11), (11, -5)]
+count = 0
+list_A = []
+for A in range(-100, 100):
+    count = 0
+    for s, t in list_s_t:
+        if(s > A) or (t > 11):
+            count += 1
+            # print("YES")
+        else:
+            pass
+            #print("NO")
+
+    if count == 4:
+        list_A.append(A)
+print(f'Ответ: {max(list_A)}')
