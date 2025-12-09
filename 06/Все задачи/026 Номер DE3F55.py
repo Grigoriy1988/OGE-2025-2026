37545ad23adfc3509488b851c7b26e428fc8e0a7
bg_19 <bg_1988@mail.ru>
2025-12-09 22:20:14 +1200

    2025

---
+++ 06/Все задачи/026 Номер DE3F55.py
@@ -0,0 +1,10 @@
+list_s_t = [(2, 5), (-2, 4), (4, 1), (-11, -5), (3, -7), (1, 8), (8, 2), (3, 0), (23, 1)]
+count = 0
+for s, t in list_s_t:
+    if (s > -3) and not (t > 5):
+        print("YES")
+    else:
+        print("NO")
+        count += 1
+
+print(f'Ответ: {count}')
