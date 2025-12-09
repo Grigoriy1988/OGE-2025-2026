000029bb6902fbd3061c5a9d0aba1354c5fa026e
bg_19 <bg_1988@mail.ru>
2025-12-10 00:48:03 +1200

    2025

---
+++ 06/Все задачи/045 Номер 0693EB.py
@@ -0,0 +1,15 @@
+list_s_t = [(-9, 11), (2, 7), (5, 12), (2, -2), (7, -9), (12, 6), (9, -1), (7, 11), (11, -5)]
+list_A = []
+for A in range(-100, 100):
+    count = 0
+    for s, t in list_s_t:
+        if(s > A) or (t > 11):
+            count += 1
+            # print("YES")
+        else:
+            pass
+            #print("NO")
+
+    if count == 6:
+        list_A.append(A)
+print(f'Ответ: {len(list_A)}')
