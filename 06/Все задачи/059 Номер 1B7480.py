3d74abfa6252961d5ba05b67d3e012326aa60311
bg_19 <bg_1988@mail.ru>
2025-12-11 23:36:17 +1200

    2025

--- 06/Все задачи/059 Номер 1B7480.py
+++ 06/Все задачи/059 Номер 1B7480.py
@@ -3,13 +3,13 @@ list_A = []
 for A in range(-100, 100):
     count = 0
     for s, t in list_s_t:
-        if (s > A) or (t > 11):
-            pass
+        if(s > A) or (t > 11):
+            count += 1
             # print("YES")
         else:
-            count += 1
+            pass
             #print("NO")
 
     if count == 3:
         list_A.append(A)
-print(f'Ответ: {min(list_A)}')
+print(f'Ответ: {max(list_A)}')
