f77c99d72d43d2e51c0d8a62ef3b3da147d02a7f
Prepod <bg_1988@mail.ru>
2026-01-21 12:18:44 +1200

    15

--- 15/Все задачи/002 Номер 0A474B.py
+++ 15/Все задачи/002 Номер 0A474B.py
@@ -2,7 +2,7 @@ m = 0
 n = int(input())
 for _ in range(n):
     a = int(input())
-    if (a % 7) % 2 != 0:
+    if (a % 7) % 2 != 0: # или (a % 7)  in [1,3,5]
         m = max(a, m)
 if m:
     print(m)
