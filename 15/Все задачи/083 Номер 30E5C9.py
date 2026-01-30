d4b54390e0a4f093af2d44538fe8ab9046c1b62f
Prepod <bg_1988@mail.ru>
2026-01-30 14:44:56 +1200

    15

---
+++ 15/Все задачи/083 Номер 30E5C9.py
@@ -0,0 +1,10 @@
+list_answer = []
+while True:
+    a = int(input())
+    if a == 0:
+        break
+    if a % 17 == 0:
+        list_answer.append(a)
+print((sum(list_answer), len(list_answer)) if list_answer else "NO")
+
+
